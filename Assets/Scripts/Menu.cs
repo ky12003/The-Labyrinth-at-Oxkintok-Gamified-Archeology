3e96a6effe567cbe15faba257045b5f139164490
Kenta Yamamoto <kenta12003@gmail.com>
2022-06-27 13:23:00 -0700

    Squashed commit of the following:
    commit f6031deebd40f222f8c24c24a8af7beac573a50d
    Author: Juan Groening <jgroening@yahoo.com>
    Date:   Fri Jun 24 16:11:17 2022 -0700
    
        Intructions Scene
    
    commit eb11f49c27283599155f74c05dd01af25c54df0d
    Author: Juan Groening <jgroening@yahoo.com>
    Date:   Fri Jun 24 15:40:10 2022 -0700
    
        Option and sound back to menu
    
    commit e095d95a707939f0ea15f17d39b03b3943cab436
    Author: Juan Groening <jgroening@yahoo.com>
    Date:   Fri Jun 24 15:00:23 2022 -0700
    
        Option
    
    commit b74b9e7d0409e3eb043e48a5ebce1f5bdecadcbb
    Author: Juan Groening <jgroening@yahoo.com>
    Date:   Fri Jun 24 14:37:10 2022 -0700
    
        Sound menu
    
    commit 6434fed0fc0a44656074a65657d53869ff124fc2
    Author: Juan Groening <jgroening@yahoo.com>
    Date:   Fri Jun 24 14:09:57 2022 -0700
    
        Menu 2
    
    commit d2951b94511acb687517407708b2156c55884cab
    Author: Juan Groening <jgroening@yahoo.com>
    Date:   Thu Jun 23 17:08:08 2022 -0700
    
        Menu
    
    commit 42185a1e65ca7cae00a80afeadb0153f9866d928
    Merge: 0f63340 c371b55
    Author: Kenta Yamamoto <55525996+ky12003@users.noreply.github.com>
    Date:   Wed Jun 22 16:40:02 2022 -0700
    
        Merge pull request #1 from ky12003/kenta
    
        Added movement

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private int index;
+    public void Play()
+    {
+        index = 1;
+        load();
+       
+    }
+
+    public void instructions()
+    {
+        index = 2;
+        load();
+    }
+
+
+    public void option()
+    {
+        index = 3;
+        load();
+    }
+
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void load()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + index);
+    }
+}
