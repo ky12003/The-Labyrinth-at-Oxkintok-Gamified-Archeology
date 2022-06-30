0d360f4c71329f73584d917d9721fecf39672108
jpgr24 <jgroening@yahoo.com>
2022-06-30 11:50:55 -0700

    Set volume

--- Assets/Scripts/SetVolume.cs
+++ Assets/Scripts/SetVolume.cs
@@ -9,6 +9,6 @@ public class SetVolume : MonoBehaviour
     public AudioMixer mixer;
     public void SetLevel(float sliderValue)
     {
-        mixer.SetFloat("MusicVol", sliderValue);
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
     }
 }
