0b294b28a74426ed508c845eaa7ab79cee30152f
Kenta Yamamoto <kenta12003@gmail.com>
2022-07-06 12:59:34 -0700

    Implemented Pause menu with Floor 1 Scene
    Added link between pause menu and main gameplay screen using main UI and ESC button.

---
+++ Assets/Scripts/Puzzle Scripts/PuzzleF1P1.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// mini notes
+// deactivate puzzle (change layer to undefined) after puzzle is done
+
+public class PuzzleF1P1 : MonoBehaviour
+{
+    // UI objects
+    // PLACEHOLDER (JUST COMMENTS FOR NOW)
+    /*
+    [SerializeField] Image numberOne
+    [SerializeField] Image numberFour
+    [SerializeField] Image
+    */
+
+    // Public variables
+    public bool puzzleIsDone = false; // for noting down if the puzzle is complete
+
+    // Private variables
+    int currStep = 1; // current step
+    int finStep = 5; // final step
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
