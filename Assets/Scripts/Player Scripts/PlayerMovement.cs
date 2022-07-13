b3fd241f3da60c96b5250c661f81d83bc5cf09fc
Kenta Yamamoto <kenta12003@gmail.com>
2022-07-13 14:00:18 -0700

    Implemented page collection and page scrolling
    Added the ability for the player to collect page objects to update their notebook. Also added the ability for the player to look through next/previous pages of their notebook using a button. Buttons appear depending on if there is a page next to/behind the current page.

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     // using tutorial: https://www.youtube.com/watch?v=_QajrabyTJc
     public CharacterController controller;
+    public float gravity = -9.81f;
 
     // for checking if player is using a popup item or not
     PopupToggle popupToggle;
@@ -14,6 +15,15 @@ public class PlayerMovement : MonoBehaviour
     // control speed
     public float speed = 4f;
 
+    // velocity
+    Vector3 velocity;
+
+    // ground check
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
     void Awake() {
         popupToggle = uiInteractor.GetComponent<PopupToggle>();
     }
@@ -21,15 +31,26 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         if (!popupToggle.popupIsOpen) {
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+            if (isGrounded && velocity.y < 0) {
+                velocity.y = -2f;
+            }
+
             // get user input for horizontal/vertical movement
             float x = Input.GetAxis("Horizontal");
             float z = Input.GetAxis("Vertical");
 
             // move along x/z axis (no y axis movement since the player cannot fly)
-            Vector3 move = transform.right * x + transform.forward * z;
+            Vector3 move = transform.right * x + transform.transform.forward * z;
 
             // move player
             controller.Move(move * speed * Time.deltaTime);
+
+            // handle gravity
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
         }
 
     }
