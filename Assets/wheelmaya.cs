db060890f14d539e56ec9ca4f248c8846b6f586b
Juan Groening <jgroening@yahoo.com>
2022-07-18 15:48:56 -0700

    Wheel ready

--- Assets/wheelmaya.cs
+++ Assets/wheelmaya.cs
@@ -8,32 +8,38 @@ public class wheelmaya : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject rul;
-    public Animation an;
-    string[] s = new string[] {"s","move"};
-    float rotationSpeed = 45;
-    Vector3 currentEulerAngles;
-    Quaternion currentRotation;
-    float x;
-    float y;
-    float z;
-    public void rotateleft()
+    
+    
+   
+   
+  
+    float zleft=0f;
+    float zright = 0f;
+    bool upleft = true;
+    bool upright = true;
+    
+    
+    public void setpartitionleftup() { rotatewheel(zleft, upleft,19f); }
+    public void setpartitionleftdown() { rotatewheel(zleft, upleft, -19f); }
+
+    public void setpartitionrightup() { rotatewheel(zleft, upleft, -20f); }
+    public void setpartitionrightdown() { rotatewheel(zleft, upleft, 20f); }
+
+
+   
+    float divide(float x)
     {
-        //
-        /*  for (int i=0; i < s.Length;i++)
-        
-
-    }*/
-        
-        an.transform.Rotate(new Vector3(0, 0, 30));
-       // Random rnd = new Random();
-
-        for (int j = 0; j < s.Length; j++)
-        {
-            an.Play(s[j]);
-            //Debug.Log(j); // returns random integers >= 10 and < 19
-        }
-        
-
+        return 360f/x;
     }
+
+    void rotatewheel(float x,bool on,float z) {
+
+        float s = divide(z);
+       
+        rul.transform.Rotate(new Vector3(0, 0, x + s)); }
+
+
+
+   
    
 }
