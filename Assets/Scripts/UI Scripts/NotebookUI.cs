281aa2af2bc3adaf54d96029438b26f223577a62
Kenta Yamamoto <kenta12003@gmail.com>
2022-07-18 14:08:18 -0700

    Implemented dynamic page collection
    Implemented the ability for users to pick up pages in whatever order they want without compromising the notebook mechanics. (eg. if player only has page 1 and 4, they can go from page 1 to 4, when previously it only detected the player having 2 pages in total, so the player could only access page 1 and 2)

--- Assets/Scripts/UI Scripts/NotebookUI.cs
+++ Assets/Scripts/UI Scripts/NotebookUI.cs
@@ -16,22 +16,29 @@ public class NotebookUI : MonoBehaviour
 
     //-----private variables-----
     static int currPage = 1; // stores current page that the user is on 
-    static int numPages = 1; // stores the number of pages in the notebook
+    static int totPages = 6; // the total number of possible pages in the notebook
+    static int newNextPage; // for storing next page
+    static int newPrevPage; // for storing previous page
+    static bool[] pagesActive = new bool[totPages]; // array of booleans; position denotes which pages are available
 
     /*----
     METHODS
     ----*/
     //-----unity methods-----
+
     void Update() {
+        newNextPage = availablePage("front");
+        newPrevPage = availablePage("back");
+
         // for updating the "next" button (set active if there is an available page after the current page)
-        if (currPage+1 <= numPages) {
+        if (newNextPage != currPage) {
             turnNextPageButton.SetActive(true);
         } else {
             turnNextPageButton.SetActive(false);
         }
 
         // for updating the "previous" button (set active if there is an available page before the current page)
-        if (currPage-1 >= 1) {
+        if (newPrevPage != currPage) {
             turnPrevPageButton.SetActive(true);
         } else {
             turnPrevPageButton.SetActive(false);
@@ -39,25 +46,84 @@ public class NotebookUI : MonoBehaviour
     }
 
     //-----public class methods-----
-    // set the number of pages available in the notebook
-    public void updateNumPages() {
-        numPages++;
+    // update the notebook pages
+    public void updateNotebook(int pageNum) {
+        // activate page corresponding to page part if the page is not already made available
+        if (pagesActive[pageNum-1] == false)
+        {
+            pagesActive[pageNum-1] = true;
+        }
+        
     }
 
     // turn to the next page
+    // TODO: UPDATE TO ACCOUNT FOR NEW PAGE SYSTEM
     public void turnToNextPage() {
         pageObjects[currPage-1].SetActive(false);
-        pageObjects[currPage].SetActive(true);
-        currPage++;
+        pageObjects[newNextPage-1].SetActive(true);
+        currPage = newNextPage;
     }
 
     // turn to the previous page
     public void turnToPrevPage() {
         pageObjects[currPage-1].SetActive(false);
-        pageObjects[currPage-2].SetActive(true);
-        currPage--;
+        pageObjects[newPrevPage-1].SetActive(true);
+        currPage = newPrevPage;
+    }
+
+    // initialize the boolean array for the pages
+    public void initializeArr()
+    {
+        pagesActive[0] = true;
+        for (int i = 1; i < totPages; i++)
+        {
+            pagesActive[i] = false;
+        }
     }
 
     //-----private class methods-----
+    // check if a page is available (returns the current page if it's not, returns the next/previous page number if there is a page available)
+    public int availablePage(string position)
+    {
+        int currentPage = currPage; // store temporary variable for the current page (since it will be updated when checking for an available page
 
+        // check for page after current page
+        if (position == "front")
+        {
+            currentPage++;
+            // check if a page after the current page is available
+            while (currentPage <= totPages)
+            {
+                if (pagesActive[currentPage - 1])
+                {
+                    // a page after the current page has been found
+                    return currentPage;
+                }
+                currentPage++;
+            }
+            // no new pages after the current page has been found
+            return currPage;
+        } 
+        // check for page before current page
+        else if (position == "back")
+        {
+            currentPage--;
+            // check if a page after the current page is available
+            while (currentPage >= 1)
+            {
+                if (pagesActive[currentPage - 1])
+                {
+                    // a page before the current page has been found
+                    return currentPage;
+                }
+                currentPage--;
+            }
+            // no new pages after the current page has been found
+            return currPage;
+        }
+        else
+        {
+            return -1; // invalid input
+        }
+    }
 }
