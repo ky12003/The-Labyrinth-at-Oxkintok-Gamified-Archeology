952679681f42b3a5cce78956aaa0374e10daa450
Kenta Yamamoto <kenta12003@gmail.com>
2022-08-05 15:22:50 -0700

    Updated indicators for new puzzle system
    Updated the indicators to change one of the materials of part of the indicator into an image of a mayan number after the completion of said puzzle.

--- Assets/Scripts/Puzzle Scripts/combinationLockPuzzle.cs
+++ Assets/Scripts/Puzzle Scripts/combinationLockPuzzle.cs
@@ -148,7 +148,7 @@ public class combinationLockPuzzle : MonoBehaviour
         puzzleObject.layer = 0;
 
         // let the event handler system know that this puzzle has been completed
-        puzzleStorage.GetComponent<FloorCompletion>().updateFloorTwoPuzzlesCompleted(puzzleCompleteIndicator1, puzzleCompleteIndicator2);
+        puzzleStorage.GetComponent<FloorCompletion>().updateFloorTwoPuzzlesCompleted(puzzleCompleteIndicator1, puzzleCompleteIndicator2, 5);
         Debug.Log("PUZZLE COMPLETE");
     }
 }
